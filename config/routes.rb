a1402f4920529868010f46d455f0bb8d4d65936c
puneet2121 <bedipuneet21@gmail.com>
2022-07-05 01:58:44 +0000

    Add catogtogries to the Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/index'
+  end
   get 'about/index'
   root to: 'products#index'
 
@@ -17,6 +20,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :create, :new]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
