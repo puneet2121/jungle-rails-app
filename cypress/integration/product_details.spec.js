45fc4587b1d79803be9e007e3a2f6d8c3e6d7c7c
puneet2121 <bedipuneet21@gmail.com>
2022-07-06 18:45:32 -0600

    Add cypress test for product and home page fix css for product page

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('http://localhost:3000/')
+  })
+  it("click on product on the page", () => {
+    cy.get(".product").first().click()
+    cy.url().should('include', '/products/2')
+  });
+ 
+  
+})
